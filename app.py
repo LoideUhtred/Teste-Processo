0f25de0be2315db2fb22766f62d95734f062797d
LoideUhtred <120178286+LoideUhtred@users.noreply.github.com>
2023-12-14 01:07:20 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,122 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from time import sleep
+import openpyxl
+
+numero_oab = 248274
+
+# entrar no site do https://tjrj.pje.jus.br/1g/ConsultaPublica/listView.seam
+
+driver = webdriver.Chrome()
+driver.get('https://tjrj.pje.jus.br/1g/ConsultaPublica/listView.seam')
+driver.get('https://tjrj.pje.jus.br/1g/ConsultaPublica/listView.seam')
+driver.get('https://tjrj.pje.jus.br/1g/ConsultaPublica/listView.seam')
+#sleep()
+
+# digitar número OAB
+campo_oab = driver.find_element(
+    By.XPATH, "//input[@id='fPP:Decoration:numeroOAB']")
+campo_oab.send_keys(numero_oab)
+sleep(2)
+
+# selecionar Estado
+
+dropdownn_estados = driver.find_element(
+    By.XPATH, "//select[@id='fPP:Decoration:estadoComboOAB']")
+opcoes_estados = Select(dropdownn_estados)
+opcoes_estados.select_by_visible_text('RJ')
+sleep(2)
+
+# Clicar em Pesquisar
+botao_pesquisar = driver.find_element(
+    By.XPATH, "//input[@id='fPP:searchProcessos']")
+botao_pesquisar.click()
+sleep(10)
+
+
+#Entrar em cada um dos processos
+processos = driver.find_elements(By.XPATH,"//b[@class='btn-block']")
+for processos in processos:
+    processos.click()
+    sleep(10)
+    janelas = driver.window_handles
+    driver.switch_to.window(janelas[-1])
+    driver.set_window_size(1280,720)
+
+    #Extrair n° do processo
+    numero_processo = driver.find_elements(By.XPATH,"//div[@class='col-sm-12 ']")
+    numero_processo = numero_processo[0]
+    numero_processo = numero_processo.text
+
+    #Extrair data
+    data_distribuicao = driver.find_elements(By.XPATH,"//div[@class='value col-sm-12 ']")
+    data_distribuicao = data_distribuicao[1]
+    data_distribuicao = data_distribuicao.text
+    sleep(5)
+
+    #Extrair e guardar todas as ultimas movimentações
+    movimentacoes = driver.find_elements(By.XPATH,'//div[@id="j_id132:processoEventoPanel_body"]//tr[contains(@class,"rich-table-row")]//td//div//div//span')
+    lista_movimentacoes = []
+    for movimentacao in movimentacoes:
+        lista_movimentacoes.append(movimentacao.text)
+
+#Guardar tudo no excel, separados por processo
+    workbook = openpyxl.load_workbook("dados.xlsx")
+    try:
+        #Codigo para inserir em pagina existente
+        workbook.create_sheet(numero_processo)
+
+        #Acessar pagina processo
+        pagina_processo = workbook[numero_processo]
+
+        #Criar nome colunas
+        pagina_processo['A1'].value = "Numero Processo"
+        pagina_processo['B1'].value = "Data Distribuição"
+        pagina_processo['C1'].value = "Movimentações"
+
+        #Adicionar numero do processo
+        pagina_processo["A2"].value = numero_processo
+        
+        #Adicionar data de distribuição
+        pagina_processo["B2"].value = data_distribuicao
+
+        #Adicionar movimentações
+        for index, linha in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
+            for celula in linha:
+                celula.value = lista_movimentacoes[index]
+        workbook.save("dados.xlsx")
+        driver.close()
+        sleep(5)
+        driver.switch_to.window(driver.window_handles[0])
+
+        
+    except Exception as error:
+        #Codigo para criar uma página do zero e inserir informações
+        workbook.create_sheet(numero_processo)
+
+        #Acessar pagina processo
+        pagina_processo = workbook[numero_processo]
+
+        #Criar nome colunas
+        pagina_processo['A1'].value = "Numero Processo"
+        pagina_processo['B1'].value = "Data Distribuição"
+        pagina_processo['C1'].value = "Movimentações"
+
+        #Adicionar numero do processo
+        pagina_processo["A2"].value = numero_processo
+        
+        #Adicionar data de distribuição
+        pagina_processo["B2"].value = data_distribuicao
+
+        #Adicionar movimentações
+        for index, linha in enumerate(pagina_processo.iter_rows(min_row=2, max_row=len(lista_movimentacoes), min_col=3, max_col=3)):
+            for celula in linha:
+                celula.value = lista_movimentacoes[list]
+        workbook.save("dados.xlsx")
+        driver.close()
+        sleep(5)
+        driver.switch_to.window(driver.window_handles[0])
+   
+    
